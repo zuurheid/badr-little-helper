1d76b8111d513fd816c5dec9f73eec4192adda07
zuurheid <zuurheid@gmail.com>
2021-01-08 22:50:12 +0100

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import { GlobalWorkerOptions, version } from "pdfjs-dist";
-GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.js`;
-
-ReactDOM.render(<App />, document.getElementById("root"));
