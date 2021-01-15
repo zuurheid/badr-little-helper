a0226b59bc759a5dbab19e2a1bcff72b5ba477f1
zuurheid <zuurheid@gmail.com>
2021-01-15 18:00:52 +0100

    add alerts for parsing files errors

--- site/src/components/Navbar/index.tsx
+++ site/src/components/Navbar/index.tsx
@@ -7,20 +7,22 @@ import { Typography } from "@material-ui/core";
 
 const Navbar = () => {
   return (
-    <AppBar
-      position="static"
-      style={{
-        backgroundColor: "rgba(255, 250, 222, 0.25)",
-        color: "rgba(0, 0, 0, 0.87)",
-      }}
-    >
-      <Toolbar>
-        <Typography variant="h4">Badr little helper</Typography>
-        <div className={s.languageSelectButton}>
-          <LanguageSelect />
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div className={s.root}>
+      <AppBar
+        position="static"
+        style={{
+          backgroundColor: "rgba(255, 250, 222, 0.25)",
+          color: "rgba(0, 0, 0, 0.87)",
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h4">Badr little helper</Typography>
+          <div className={s.languageSelectButton}>
+            <LanguageSelect />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
